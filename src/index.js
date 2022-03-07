f963f061e12a4e3b9e2d67bb2c16c3499e507679
陶鸿杰 <taohongjie@bytedance.com>
2022-03-07 20:10:37 +0800

    feat: img

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import "./style.css";
+import Icon from "./icon.jpeg";
 
 function component() {
   const element = document.createElement("div");
@@ -7,6 +8,11 @@ function component() {
   // _ lodash
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
